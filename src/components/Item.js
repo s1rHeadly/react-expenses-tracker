7961abbacc46a386329362a65dce9b87d5001162
davidmchale <dmchale@squiz.net>
2023-12-19 10:11:30 +1100

    read me updates

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 const Item = ({item}) => {
-  const {title, amount} = item;
+  const {title, amount, id} = item;
+
   
 
   const signIcon = amount < 0 ? 'minus' : 'plus';
 
   return (
-    <li className={signIcon}>
+    <li className={signIcon} data-id={id}>
     {title} <span>{amount}</span>
     <button className="delete-btn">X</button>
   </li>
